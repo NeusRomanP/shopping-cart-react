a027e9576b1770b33de4a763697afff4b54f23a7
Neus Roman <neusroman@hotmail.com>
2025-02-03 19:21:58 +0100

    add total products and total price

--- src/components/ProductsCart.tsx
+++ src/components/ProductsCart.tsx
@@ -17,9 +17,31 @@ export function ProductsCart ({products, date ,changeAmount, deleteProduct}: Pro
       <main className="products-list">
         {
           products.size
-          ? Array.from(products, ([key, value]) => (
-            <CartItem key={key} product={value} changeAmount={changeAmount} deleteProduct={deleteProduct}/>
-          ))
+          ? (
+            <>
+              <div className="items">
+                {
+                  Array.from(products, ([key, value]) => (
+                    <CartItem key={key} product={value} changeAmount={changeAmount} deleteProduct={deleteProduct}/>
+                  ))
+                }
+              </div>
+              <div className="totals">
+                <span>
+                  <strong>Total productos: </strong>
+                  <span className="total">
+                    {Array.from(products.values()).reduce((acc, item) => acc + item.amount, 0)}
+                  </span>
+                </span>
+                <span>
+                  <strong>Precio total: </strong>
+                  <span className="total">
+                    ${Array.from(products.values()).reduce((acc, item) => acc + item.amount * item.product.price, 0).toFixed(2)}
+                  </span>
+                </span>
+              </div>
+            </>
+          )
           : (
             <p>No hay productos en el carrito</p>
           )
